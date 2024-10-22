2788aa90d0ac52ed65cfe3c4146b188f71755080
dar3valo <wongdarren516@gmail.com>
2024-10-22 17:18:05 +0800

    Email bar colour modified to dark

--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import EmailForm from './components/emailForm';
 import Tabs from './components/tabs';
-import NavBar from './components/navBar';
+// import NavBar from './components/navBar';
 import EmailBar from './components/emailBar'; // Ensure this is imported
 import './app.css';
 
 function App() {
    return (
        <div className="App">
-           <NavBar />
+
            <div className="main-content">
                <Tabs />
                <div className="email-section">
+                <EmailBar /> {/* Ensure this component is rendered */}
                    <EmailForm />
-                   <EmailBar /> {/* Ensure this component is rendered */}
                </div>
            </div>
        </div>
